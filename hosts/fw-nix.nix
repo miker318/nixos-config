7b6161b73ac3f77aaaa23241c2f273f80d645039
Michael Rose <mike@erose.org>
2025-01-10 17:00:25 -0500

    Workaround for virtualbox error on 6.12 kernel

--- hosts/fw-nix.nix
+++ hosts/fw-nix.nix
@@ -39,6 +39,9 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  #workaround for virtualbox
+  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
